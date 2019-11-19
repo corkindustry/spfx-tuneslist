d3070aaebf0eabcd4de70ce3f38d348afc97529e
Jonah <falsefriend13@gmail.com>
2019-11-19 12:26:51 -0500

    upgraded SPFx version and changed webpart content

--- src/webparts/tuneslist/TuneslistWebPart.ts
+++ src/webparts/tuneslist/TuneslistWebPart.ts
@@ -1,32 +1,28 @@
-import { Version } from '@microsoft/sp-core-library';
+import { Version } from "@microsoft/sp-core-library";
 import {
   BaseClientSideWebPart,
   IPropertyPaneConfiguration,
   PropertyPaneTextField
-} from '@microsoft/sp-webpart-base';
-import { escape } from '@microsoft/sp-lodash-subset';
+} from "@microsoft/sp-webpart-base";
+import { escape } from "@microsoft/sp-lodash-subset";
 
-import styles from './TuneslistWebPart.module.scss';
-import * as strings from 'TuneslistWebPartStrings';
+import styles from "./TuneslistWebPart.module.scss";
+import * as strings from "TuneslistWebPartStrings";
 
 export interface ITuneslistWebPartProps {
   description: string;
 }
 
-export default class TuneslistWebPart extends BaseClientSideWebPart<ITuneslistWebPartProps> {
-
+export default class TuneslistWebPart extends BaseClientSideWebPart<
+  ITuneslistWebPartProps
+> {
   public render(): void {
     this.domElement.innerHTML = `
-      <div class="${ styles.tuneslist }">
-        <div class="${ styles.container }">
-          <div class="${ styles.row }">
-            <div class="${ styles.column }">
-              <span class="${ styles.title }">Welcome to SharePoint!</span>
-              <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
-              <p class="${ styles.description }">${escape(this.properties.description)}</p>
-              <a href="https://aka.ms/spfx" class="${ styles.button }">
-                <span class="${ styles.label }">Learn more</span>
-              </a>
+      <div class="${styles.tuneslist}">
+        <div class="${styles.container}">
+          <div class="${styles.row}">
+            <div class="${styles.column}">
+              <h1>Hi</h1>
             </div>
           </div>
         </div>
@@ -34,7 +30,7 @@ export default class TuneslistWebPart extends BaseClientSideWebPart<ITuneslistWe
   }
 
   protected get dataVersion(): Version {
-    return Version.parse('1.0');
+    return Version.parse("1.0");
   }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
@@ -48,7 +44,7 @@ export default class TuneslistWebPart extends BaseClientSideWebPart<ITuneslistWe
             {
               groupName: strings.BasicGroupName,
               groupFields: [
-                PropertyPaneTextField('description', {
+                PropertyPaneTextField("description", {
                   label: strings.DescriptionFieldLabel
                 })
               ]
